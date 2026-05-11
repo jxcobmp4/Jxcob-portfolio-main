89f1571f642dc09ece9fe8497bba1f7cd3a5b6d2
Jxcob <dopjacob@gmail.com>
2026-05-11 17:45:15 -0500

    v3

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -43,31 +43,36 @@ export const ABOUT = {
 export const VIDEOS = [
   {
     id: 1,
+    src: "https://hecmqwtjkouwedvbpipn.supabase.co/storage/v1/object/public/Jxcob-%20Portafolio/V1%20DrP%20(1).mp4",
+    title: "V1 DrP",
+  },
+  {
+    id: 2,
     src: "https://hecmqwtjkouwedvbpipn.supabase.co/storage/v1/object/public/Jxcob-%20Portafolio/V2.mp4",
     title: "V2",
   },
   {
-    id: 2,
+    id: 3,
     src: "https://hecmqwtjkouwedvbpipn.supabase.co/storage/v1/object/public/Jxcob-%20Portafolio/Anecdota%201.mp4",
     title: "Anecdota 1",
   },
   {
-    id: 3,
+    id: 4,
     src: "https://hecmqwtjkouwedvbpipn.supabase.co/storage/v1/object/public/Jxcob-%20Portafolio/Anecdota%202%20.mp4",
     title: "Anecdota 2",
   },
   {
-    id: 4,
+    id: 5,
     src: "https://hecmqwtjkouwedvbpipn.supabase.co/storage/v1/object/public/Jxcob-%20Portafolio/Cabarico-top%203.mp4",
     title: "Cabarico Top 3",
   },
   {
-    id: 5,
+    id: 6,
     src: "https://hecmqwtjkouwedvbpipn.supabase.co/storage/v1/object/public/Jxcob-%20Portafolio/Final-DuoMillonario.mp4",
     title: "Final Duo Millonario",
   },
   {
-    id: 6,
+    id: 7,
     src: "https://hecmqwtjkouwedvbpipn.supabase.co/storage/v1/object/public/Jxcob-%20Portafolio/Errores.mp4",
     title: "Errores",
   }
